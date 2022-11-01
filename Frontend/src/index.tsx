a15930b8e7e04eb06135dff19d8645e6bb064da7
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-11-01 15:08:57 +0800

    make frontend folder

---
+++ Frontend/src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "./index.css";
+
+import { ContextProvider } from "./contexts/ContextProvider";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>
+);
