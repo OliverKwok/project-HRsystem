02e115aa68e5d2812da5a111d56a0ea93e8ac473
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-11-08 17:35:07 +0800

    frontend_basic_new_employee_form

--- frontend/src/pages/Employee.tsx
+++ frontend/src/pages/Employee.tsx
@@ -1,31 +1,47 @@
-import React, { useEffect } from 'react'
-import { Button, Form } from 'react-bootstrap'
-import { useForm } from 'react-hook-form'
+import React, { useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 type FormState = {
-  name: string
-  color: string
-  gender: string
-}
+  firstName: string;
+  lastName: string;
+  chineseName: string;
+  alias: string;
+  mobileLocation: string;
+  mobile: string;
+  personalEmail: string;
+  workEmail: string;
+  birthday: string;
+  hkid: string;
+  gender: string;
+};
 
-export default function Employee () {
+export default function Employee() {
   const { register, handleSubmit, watch } = useForm<FormState>({
     defaultValues: {
-      name: '',
-      color: '#c0ffee',
-      gender: '',
+      firstName: "",
+      lastName: "",
+      chineseName: "",
+      alias: "",
+      mobileLocation: "852",
+      mobile: "",
+      personalEmail: "",
+      workEmail: "",
+      birthday: "",
+      hkid: "",
+      gender: "",
     },
-  })
+  });
 
   useEffect(() => {
-    let sub = watch(data => {
-      console.log('update form data:', data)
-    })
-    return () => sub.unsubscribe()
-  }, [watch])
+    let sub = watch((data) => {
+      console.log("update form data:", data);
+    });
+    return () => sub.unsubscribe();
+  }, [watch]);
 
   function submit(data: FormState) {
-    console.log('submit form data:', data)
+    console.log("submit form data:", data);
   }
 
   return (
@@ -33,26 +49,58 @@ export default function Employee () {
       <h1>New Employee</h1>
       <Form onSubmit={handleSubmit(submit)}>
         <Form.Group>
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" {...register('name')} />
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="text" {...register("firstName")} />
         </Form.Group>
         <Form.Group>
-          <Form.Label>Color</Form.Label>
-          <Form.Control
-            type="color"
-            {...register('color')}
-            className="d-inline"
-          />
-          <Form.Text className="text-muted">
-            Custom the background color
-          </Form.Text>
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="text" {...register("lastName")} />
         </Form.Group>
-        
+        <Form.Group>
+          <Form.Label>Chinese Name</Form.Label>
+          <Form.Control type="text" {...register("chineseName")} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Alias</Form.Label>
+          <Form.Control type="text" {...register("alias")} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Mobile Location</Form.Label>
+          <Form.Control type="text" {...register("mobileLocation")} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Mobile</Form.Label>
+          <Form.Control type="text" {...register("mobile")} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Personal Email</Form.Label>
+          <Form.Control type="text" {...register("personalEmail")} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Work Email</Form.Label>
+          <Form.Control type="text" {...register("workEmail")} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Birthday</Form.Label>
+          <Form.Control type="text" {...register("birthday")} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>HKID</Form.Label>
+          <Form.Control type="text" {...register("hkid")} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Gender</Form.Label>
+        <select {...register("gender")}>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+
+        </Form.Group>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
       </Form>
     </div>
-  )
+  );
 }
-
