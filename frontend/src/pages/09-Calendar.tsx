44a151e0fc5dd666dca77d615832c996da70e0df
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-11-09 15:50:30 +0800

    update_calendar_use_state

--- frontend/src/pages/09-Calendar.tsx
+++ frontend/src/pages/09-Calendar.tsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import React, { useRef, useState } from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 
-// const Calendar = () => {
-//   return (
-//     <div>09-Calendar</div>
-//   )
-// }
+const Calendar = () => {
+  const [event, setEvent] = useState([
+    { title: "event 1", date: "2022-11-09" },
+    { title: "event 2", date: "2022-11-11" },
+    { title: "event 3", date: "2022-11-13" },
+  ]);
 
-// export default Calendar
+  return (
+    <>
+      {/* <div>
+        <input type="text" />
+        <input type="text" />
+      </div> */}
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        weekends={false}
+        events={event}
+      />
+    </>
+  );
+};
 
-export default class DemoApp extends React.Component {
-    render() {
-
-        
-
-      return (
-        <FullCalendar
-          plugins={[ dayGridPlugin ]}
-          initialView="dayGridMonth"
-          weekends={false}
-          events={[
-            { title: 'event 1', date: '2022-11-09' },
-            { title: 'event 2', date: '2022-11-11' }
-          ]}
-          />
-      )
-    }
-  }
+export default Calendar;
