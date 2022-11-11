78630a89c5fdcbb7447b2294aab90a5ead9c1c4d
sherie <mayinlam@gmail.com>
2022-11-11 13:12:17 +0800

    drag and drop list view

---
+++ frontend/src/pages/drag-and-drop/helpers.tsx
@@ -0,0 +1,7 @@
+export const reorder = (list: any, startIndex: any, endIndex: any) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
