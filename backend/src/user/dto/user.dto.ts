a0b8fb83e7aa8300fdbb8c2e0a5eba274bd50643
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-11-11 15:29:18 +0800

    merge

--- backend/src/user/dto/user.dto.ts
+++ backend/src/user/dto/user.dto.ts
@@ -1,9 +1,6 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  //   @IsNotEmpty()
-  //   @MinLength(3)
-  //   username: string;
 
   @IsNotEmpty()
   // @IsEmail()
