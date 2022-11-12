271cdbb8e1f49b1580ccda8a941696d598ef7961
sherie <mayinlam@gmail.com>
2022-11-12 23:01:30 +0800

    draft grade page

--- frontend/src/pages/02b-Grade.tsx
+++ frontend/src/pages/02b-Grade.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import "../styles/02b-Grade.css"
 
 const Grade = () => {
   return (
-    <div>02b-Grade</div>
-  )
-}
+    <>
+      <h1>Grades</h1>
+      <button>Add New Grade</button>
+      <div className="mainTable">
+        <div className="gradeCol">Grade </div>
+        <div className="titleCol">Titles</div>
+        <div className="employeeCol">Employees</div>
+      </div>
+    </>
+  );
+};
 
-export default Grade
+export default Grade;
