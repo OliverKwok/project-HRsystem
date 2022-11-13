2e2d47a37e9bf99bc8e87fd90b2cd4ed1e32f4e1
sherie <mayinlam@gmail.com>
2022-11-13 13:58:43 +0800

    add grade grouping table

--- frontend/src/pages/02b-Grade.tsx
+++ frontend/src/pages/02b-Grade.tsx
@@ -1,18 +1,85 @@
-import React from "react";
-import "../styles/02b-Grade.css"
+import { ColumnDirective, ColumnsDirective } from "@syncfusion/ej2-react-grids";
+import {
+  Grid,
+  GridComponent,
+  Group,
+  Page,
+  LazyLoadGroup,
+  GroupSettingsModel,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+import * as React from "react";
+import { data } from "./02b-grade-datasource";
+import "../styles/02b-Grade.css";
 
-const Grade = () => {
+export default function Grade() {
+  let grid: Grid | null;
+  let groupOptions: GroupSettingsModel = {
+    enableLazyLoading: true,
+    columns: ["ProductName", "CustomerName"],
+    showDropArea: false,
+  };
+
   return (
-    <>
-      <h1>Grades</h1>
-      <button>Add New Grade</button>
-      <div className="mainTable">
-        <div className="gradeCol">Grade </div>
-        <div className="titleCol">Titles</div>
-        <div className="employeeCol">Employees</div>
-      </div>
-    </>
+    <div>
+      <GridComponent
+        dataSource={data}
+        allowPaging={true}
+        allowGrouping={true}
+        groupSettings={groupOptions}
+        height={240}
+        ref={(g) => (grid = g)}
+      >
+        <ColumnsDirective>
+          <ColumnDirective
+            field="OrderID"
+            headerText="Order ID"
+            textAlign="Right"
+            width="120"
+          />
+          <ColumnDirective
+            field="ProductName"
+            headerText="Product Name"
+            width="160"
+          />
+          <ColumnDirective
+            field="ProductID"
+            headerText="Product ID"
+            textAlign="Right"
+            width="120"
+          />
+          <ColumnDirective
+            field="CustomerID"
+            headerText="Customer ID"
+            width="120"
+          />
+          <ColumnDirective
+            field="CustomerName"
+            headerText="Customer Name"
+            width="160"
+          />
+        </ColumnsDirective>
+        <Inject services={[Page, Group, LazyLoadGroup]} />
+      </GridComponent>
+    </div>
   );
-};
+}
 
-export default Grade;
+// import React from "react";
+// import "../styles/02b-Grade.css"
+
+// const Grade = () => {
+//   return (
+//     <>
+//       <h1>Grades</h1>
+//       <button>Add New Grade</button>
+//       <div className="mainTable">
+//         <div className="gradeCol">Grade </div>
+//         <div className="titleCol">Titles</div>
+//         <div className="employeeCol">Employees</div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Grade;
