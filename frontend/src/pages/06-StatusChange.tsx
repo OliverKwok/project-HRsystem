a7b4b03e149c99958b2d2fd26a4a50e1012b5c5e
sherie <mayinlam@gmail.com>
2022-11-17 17:18:27 +0800

    add carousel to statuschange page

--- frontend/src/pages/06-StatusChange.tsx
+++ frontend/src/pages/06-StatusChange.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StatusChangeCard from "../components/06-statusChangeCard";
+import { Carousel } from "primereact/carousel";
 
 interface statusCard {
   id: string;
@@ -12,7 +13,6 @@ interface statusCard {
 }
 
 export default function StatusChange() {
-
   const statuses: statusCard[] = [
     {
       id: "01",
@@ -34,11 +34,69 @@ export default function StatusChange() {
       bgcolor: "#6a1b9a",
       completed: 60,
     },
+    {
+      id: "03",
+      status: "Resignation",
+      profilepic:
+        "https://play-lh.googleusercontent.com/i1qvljmS0nE43vtDhNKeGYtNlujcFxq72WAsyD2htUHOac57Z9Oiew0FrpGKlEehOvo=w240-h480-rw",
+      person: "Katherine Wong",
+      position: "Finance Officer",
+      bgcolor: "#6a1b9a",
+      completed: 60,
+    },
+    {
+      id: "04",
+      status: "Resignation",
+      profilepic:
+        "https://play-lh.googleusercontent.com/i1qvljmS0nE43vtDhNKeGYtNlujcFxq72WAsyD2htUHOac57Z9Oiew0FrpGKlEehOvo=w240-h480-rw",
+      person: "Charlotte Cakad",
+      position: "CEO",
+      bgcolor: "#6a1b9a",
+      completed: 10,
+    },
+    {
+      id: "05",
+      status: "Resignation",
+      profilepic:
+        "https://play-lh.googleusercontent.com/i1qvljmS0nE43vtDhNKeGYtNlujcFxq72WAsyD2htUHOac57Z9Oiew0FrpGKlEehOvo=w240-h480-rw",
+      person: "David Manhill",
+      position: "Technician",
+      bgcolor: "#6a1b9a",
+      completed: 50,
+    },
   ];
+
+  // const responsiveOptions = [
+  //   {
+  //     breakpoint: "1024px",
+  //     numVisible: 4,
+  //     numScroll: 4,
+  //   },
+  //   {
+  //     breakpoint: "600px",
+  //     numVisible: 2,
+  //     numScroll: 2,
+  //   },
+  //   {
+  //     breakpoint: "480px",
+  //     numVisible: 1,
+  //     numScroll: 1,
+  //   },
+  // ];
 
   return (
     <>
-      {statuses.map((item, id) => (
+      <div className="card">
+        <Carousel
+          value={statuses}
+          itemTemplate={StatusChangeCard}
+          numVisible={4}
+          numScroll={1}
+          // responsiveOptions={responsiveOptions}
+        />
+      </div>
+      {/* 
+      {statuses.map((item: any, id) => (
         <StatusChangeCard
           key={id}
           status={item.status}
@@ -47,7 +105,7 @@ export default function StatusChange() {
           person={item.person}
           position={item.position}
         />
-      ))}
+      ))} */}
     </>
   );
 }
