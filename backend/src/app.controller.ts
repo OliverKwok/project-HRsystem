d6de29de9425f0ba7d687dc114effa502a1e3e36
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-11-17 15:56:28 +0800

    jwt strategy works

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
-// import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
@@ -22,9 +22,9 @@ export class AppController {
     return this.authService.login(req.user);
   }
 
-  // @UseGuards(JwtAuthGuard)
-  // @Get('profile')
-  // async profile(@Request() req) {
-  //   return req.user;
-  // }
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async profile(@Request() req) {
+    return req.user;
+  }
 }
