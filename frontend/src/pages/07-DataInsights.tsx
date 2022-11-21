f0f69f549648aa267fb4a44e3866489b900c20fb
James Siu <siukinpo@gmail.com>
2022-11-21 11:47:01 +0800

    for pull

--- frontend/src/pages/07-DataInsights.tsx
+++ frontend/src/pages/07-DataInsights.tsx
@@ -1,22 +1,34 @@
 import React, { useState } from "react";
 import { Bar } from "react-chartjs-2";
-// import Barchart from "../components/07-Barchart";
+import Barchart from "../components/07-Barchart";
 import userData from "../jsonFiles/DataInsights.json";
 
+// type DataSetType = {
+//   label: String;
+//   data: Number[];
+// };
+
+// type UserDataType = {
+//   labels: Number[];
+//   datasets: DataSetType[];
+// };
+
 const DataInsights = () => {
-  // const [{ userData }, setUserData] = useState<any>({
-  //   labels: userData.map((data: any) => data.year),
-  //   datasets: [
-  //     {
-  //       label: "Users Gained",
-  //       data: userData.map((data: any) => data.userGain),
-  //     },
-  //   ],
-  // });
+  const [userDataJson, setUserDataJson] = useState({
+    labels: userData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: userData.map((data) => data.userGain),
+      },
+    ],
+  });
 
   return (
     <>
-      <div>{/* <Barchart chartData={userData} /> */}</div>;
+      <div>
+        <Barchart chartData={userData} />
+      </div>
     </>
   );
 };
