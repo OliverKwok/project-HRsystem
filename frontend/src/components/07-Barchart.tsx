80387a29b5870f2945b50fec7fe023bdb0d2065b
James Siu <siukinpo@gmail.com>
2022-11-21 15:41:41 +0800

    for pull

--- frontend/src/components/07-Barchart.tsx
+++ frontend/src/components/07-Barchart.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
-// import { Chart as ChartJS } from "chart.js/auto";
+import { Chart as ChartJS } from "chart.js/auto";
 
 function Barchart({ chartData }: { chartData: any }) {
   return <Bar data={chartData} />;
--- frontend/src/pages/07-DataInsights.tsx
+++ frontend/src/pages/07-DataInsights.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-// import { Bar } from "react-chartjs-2";
-import Barchart from "../components/07-Barchart";
+import { Bar, Doughnut } from "react-chartjs-2";
+// import Barchart from "../components/07-Barchart";
 import userData from "../jsonFiles/DataInsights.json";
-
 // type DataSetType = {
 //   label: String;
 //   data: Number[];
@@ -13,6 +12,37 @@ import userData from "../jsonFiles/DataInsights.json";
 //   datasets: DataSetType[];
 // };
 
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: ["3", "2", "1", "3", "2", "1", "3"],
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: ["1", "2", "3", "1", "2", "3", "100"],
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
 const DataInsights = () => {
   const [userDataChart, setUserDataJson] = useState({
     labels: userData.map((data) => data.year),
@@ -20,22 +50,24 @@ const DataInsights = () => {
       {
         label: "Users Gained",
         data: userData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
+        // backgroundColor: [
+        //   "rgba(75,192,192,1)",
+        //   "#ecf0f1",
+        //   "#50AF95",
+        //   "#f3ba2f",
+        //   "#2a71d0",
+        // ],
+        // borderColor: "black",
+        // borderWidth: 2,
       },
     ],
   });
 
   return (
     <div>
-      <Barchart chartData={userDataChart} />
+      {/* <Barchart chartData={userDataChart} /> */}
+      <h1>hihi</h1>
+      <Bar options={options} data={data} />
     </div>
   );
 };
