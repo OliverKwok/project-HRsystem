7d8c59e994bfdecdb6abc175d863c38a72dbd705
James Siu <siukinpo@gmail.com>
2022-11-21 12:18:58 +0800

    for database

--- frontend/src/pages/07-DataInsights.tsx
+++ frontend/src/pages/07-DataInsights.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Bar } from "react-chartjs-2";
+// import { Bar } from "react-chartjs-2";
 import Barchart from "../components/07-Barchart";
 import userData from "../jsonFiles/DataInsights.json";
 
@@ -14,22 +14,29 @@ import userData from "../jsonFiles/DataInsights.json";
 // };
 
 const DataInsights = () => {
-  const [userDataJson, setUserDataJson] = useState({
+  const [userDataChart, setUserDataJson] = useState({
     labels: userData.map((data) => data.year),
     datasets: [
       {
         label: "Users Gained",
         data: userData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
       },
     ],
   });
 
   return (
-    <>
-      <div>
-        <Barchart chartData={userData} />
-      </div>
-    </>
+    <div>
+      <Barchart chartData={userDataChart} />
+    </div>
   );
 };
 
