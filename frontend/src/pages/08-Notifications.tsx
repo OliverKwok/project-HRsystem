4fefd42dbac3a4e6af2df2da85c4d46c32d48344
sherie <mayinlam@gmail.com>
2022-11-22 11:11:43 +0800

    add company announcement page

--- frontend/src/pages/08-Notifications.tsx
+++ frontend/src/pages/08-Notifications.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
 
 export default function Notifications() {
- 
+  const { register, handleSubmit } = useForm();
+  const handleNotifications = (data: any) => console.log(data);
 
   return (
-    <>
-   Notifications
-    </>
-  )
+    <form onSubmit={handleSubmit(handleNotifications)}>
+      <div>
+        <h2>Create Company Announcement</h2>
 
+        <textarea placeholder="write your message here" {...register("name")} />
+        <input type="date" {...register("broadcast date")} />
+        <input type="time" {...register("broadcast time")} />
+      </div>
+      {/* <div>
+        <label>Email</label>
+        <input type="email" {...register('email')} />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" {...register('password')} />
+      </div> */}
+      <button>Broadcast</button>
+    </form>
+  );
 }
