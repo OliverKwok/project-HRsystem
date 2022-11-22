0ecce4679ea0c7b723057883986c6836fac7d2eb
sherie <mayinlam@gmail.com>
2022-11-22 11:53:59 +0800

    add prev announcement

--- frontend/src/pages/08-Notifications.tsx
+++ frontend/src/pages/08-Notifications.tsx
@@ -1,28 +1,43 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { Splitter, SplitterPanel } from "primereact/splitter";
+import "../styles/08-notifications.css";
 
 export default function Notifications() {
   const { register, handleSubmit } = useForm();
   const handleNotifications = (data: any) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(handleNotifications)}>
-      <div>
-        <h2>Create Company Announcement</h2>
+    <div className="card">
+      <Splitter style={{ height: "300px" }} className="mb-5">
+        <SplitterPanel className="flex align-items-center justify-content-center">
+          <form onSubmit={handleSubmit(handleNotifications)}>
+            <div>
+              <h2>Create Company Announcement</h2>
+              <div>
+                <textarea
+                  placeholder="write your message here"
+                  {...register("name")}
+                />
+              </div>
+              <div>
+                <input type="date" {...register("broadcast date")} />
 
-        <textarea placeholder="write your message here" {...register("name")} />
-        <input type="date" {...register("broadcast date")} />
-        <input type="time" {...register("broadcast time")} />
-      </div>
-      {/* <div>
-        <label>Email</label>
-        <input type="email" {...register('email')} />
-      </div>
-      <div>
-        <label>Password</label>
-        <input type="password" {...register('password')} />
-      </div> */}
-      <button>Broadcast</button>
-    </form>
+                <input type="time" {...register("broadcast time")} />
+              </div>
+            </div>
+            <button>Broadcast</button>
+          </form>
+        </SplitterPanel>
+        <SplitterPanel className="flex align-items-center justify-content-center">
+          <h3>Previous Announcements</h3>
+          <div className="prevAnn">
+            <p>Date:</p>
+            <p>Time:</p>
+            <p>Message:</p>
+          </div>
+        </SplitterPanel>
+      </Splitter>
+    </div>
   );
 }
