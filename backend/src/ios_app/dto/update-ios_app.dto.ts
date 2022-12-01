917251ad1d0db395b1be1c5030049a8153cdd3d4
James Siu <siukinpo@gmail.com>
2022-12-01 11:27:30 +0800

    fixed app login

--- backend/src/ios_app/dto/update-ios_app.dto.ts
+++ backend/src/ios_app/dto/update-ios_app.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNumber } from 'class-validator';
 import { CreateIosAppDto } from './create-ios_app.dto';
 
-export class UpdateIosAppDto extends PartialType(CreateIosAppDto) {}
+export class UpdateIosAppDto extends PartialType(CreateIosAppDto) {
+  @IsNumber()
+  id: number;
+}
