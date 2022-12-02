d41f447b86b545a53d749726d775aa032785944e
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-12-02 13:10:23 +0800

    simple payroll list from json

---
+++ backend/src/payroll/payroll.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PayrollService } from './payroll.service';
+import { CreatePayrollDto } from './dto/create-payroll.dto';
+import { UpdatePayrollDto } from './dto/update-payroll.dto';
+
+@Controller('payroll')
+export class PayrollController {
+  constructor(private readonly payrollService: PayrollService) {}
+
+  @Post()
+  create(@Body() createPayrollDto: CreatePayrollDto) {
+    return this.payrollService.create(createPayrollDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.payrollService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.payrollService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePayrollDto: UpdatePayrollDto) {
+    return this.payrollService.update(+id, updatePayrollDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.payrollService.remove(+id);
+  }
+}
