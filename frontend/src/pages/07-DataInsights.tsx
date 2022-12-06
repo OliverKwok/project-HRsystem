a7268af41737f8557920260fdcefb2bf1ffd5ad6
Oliver Kwok <hk.oliver.kwok@gmail.com>
2022-12-06 13:05:41 +0800

    add sample polar chart

--- frontend/src/pages/07-DataInsights.tsx
+++ frontend/src/pages/07-DataInsights.tsx
@@ -1,69 +1,33 @@
 import React, { useState } from "react";
 import { Bar, Doughnut } from "react-chartjs-2";
-import Barchart from "../components/07-Barchart";
-import LineChart from "../components/07-LineChart";
+// import userData from "../jsonFiles/DataInsights.json";
 import DoughnutChart from "../components/07-DoughnutChart";
-import userData from "../jsonFiles/DataInsights.json";
 import VerticalBarChart from "../components/07-VerticalBarChart";
+import PolarAreaChart from "../components/07-PolarAreaChart";
 
-const DataInsights = () => {
-  const [dataGain, setDataGain] = useState({
-    labels: userData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: userData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-  const [dataLost, setDataLost] = useState({
-    labels: userData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Lost",
-        data: userData.map((data) => data.userLost),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-  return (
-    <>
-      <div className="two-column-grid">
-        <div>
-          <div>Job Status of Employees</div>
-          <DoughnutChart />
-        </div>
-        <div>
-          <div>Year of Service</div>
-          <VerticalBarChart />
-        </div>
-        <div>
+const DataInsights = () => (
+  /* const [dataGain, setDataGain] = useState({*/ /*   labels: userData.map((data) => data.year),*/ /*   datasets: [*/ /*     {*/ /*       label: "Users Gained",*/ /*       data: userData.map((data) => data.userGain),*/ /*       backgroundColor: [*/ /*         "rgba(75,192,192,1)",*/ /*         "#ecf0f1",*/ /*         "#50AF95",*/ /*         "#f3ba2f",*/ /*         "#2a71d0",*/ /*       ],*/ /*       borderColor: "black",*/ /*       borderWidth: 2,*/ /*     },*/ /*   ],*/ /* });*/ /* const [dataLost, setDataLost] = useState({*/ /*   labels: userData.map((data) => data.year),*/ /*   datasets: [*/ /*     {*/ /*       label: "Users Lost",*/ /*       data: userData.map((data) => data.userLost),*/ /*       backgroundColor: [*/ /*         "rgba(75,192,192,1)",*/ /*         "#ecf0f1",*/ /*         "#50AF95",*/ /*         "#f3ba2f",*/ /*         "#2a71d0",*/ /*       ],*/ /*       borderColor: "black",*/ /*       borderWidth: 2,*/ /*     },*/ /*   ],*/ /* });*/ <>
+    <div className="two-column-grid">
+      <div>
+        <div>Job Status of Employees</div>
+        <DoughnutChart />
+      </div>
+      <div>
+        <div>Human Cost by Department</div>
+        <PolarAreaChart />
+      </div>
+      <div>
+        <div>Year of Service</div>
+        <VerticalBarChart />
+      </div>
+      {/* <div>
           <Barchart chartData={dataGain} />
         </div>
         <div>
           <LineChart chartData={dataLost} />
-        </div>
-      </div>
-    </>
-  );
-};
+        </div> */}
+    </div>
+  </>
+);
 
 export default DataInsights;
